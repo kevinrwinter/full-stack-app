1cd049fc84cce602195fddf0b5df26dd116287c8
Kevin Winter <kevin.winter@edu.bc.fi>
2022-04-20 15:31:26 +0300

    Up to date with classroom progress - errors still to fix

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,5 +1,44 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 export function CreateUser() {
-  return <h1>Create Users</h1>;
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState({});
+
+  // Collect input data
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post("http://localhost:8005/api/", inputs).then(function (response) {
+      console.log(response.data);
+      navigate("/");
+    });
+    console.log(inputs);
+  };
+
+  return (
+    <div>
+      <h1>Create Users</h1>
+      <form onSubmit={handleSubmit} action="" method="">
+        <label htmlFor="name">Name:</label>
+        <input type="text" name="name" id="name" onChange={handleChange} />
+        <br />
+        <label htmlFor="email">Email:</label>
+        <input type="text" name="email" id="email" onChange={handleChange} />
+        <br />
+        <label htmlFor="mobile">Mobile:</label>
+        <input type="text" name="mobile" id="mobile" onChange={handleChange} />
+        <br />
+        <button>save</button>
+      </form>
+    </div>
+  );
 }
 
 // React Router DOM
